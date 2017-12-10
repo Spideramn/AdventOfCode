10618d6d2aac72f92fe027db2b932653d116a793
Spideramn <Spideramn@users.noreply.github.com>
2017-12-10 19:16:00 +0100

    Day 9

--- AdventOfCode2017/Days/Day09.cs
+++ AdventOfCode2017/Days/Day09.cs
@@ -9,12 +9,83 @@ namespace AdventOfCode2017.Days
 
 		public override object RunPart1()
 		{
-			return null;
+			var skipNext = false;
+			var inGarbage = false;
+			var totalScore = 0;
+			var groupScore = 0;
+			foreach (var c in Input)
+			{
+				if (skipNext)
+				{
+					skipNext = false;
+					continue;
+				}
+				switch (c)
+				{
+					case '<':
+						inGarbage = true;
+						break;
+
+					case '>':
+						inGarbage = false;
+						break;
+
+					case '!':
+						skipNext = true;
+						break;
+
+					case '{':
+						if (!inGarbage)
+							groupScore++;
+						break;
+
+					case '}':
+						if (!inGarbage)
+						{
+							totalScore += groupScore;
+							groupScore--;
+						}
+						break;
+				}
+			}
+			return totalScore;
 		}
 
 		public override object RunPart2()
 		{
-			return null;
+			var skipNext = false;
+			var inGarbage = false;
+			var garbage = 0;
+			foreach (var c in Input)
+			{
+				if (skipNext)
+				{
+					skipNext = false;
+					continue;
+				}
+				switch (c)
+				{
+					case '<':
+						if (inGarbage)
+							garbage++;
+						inGarbage = true;
+						break;
+
+					case '>':
+						inGarbage = false;
+						break;
+
+					case '!':
+						skipNext = true;
+						break;
+
+					default:
+						if (inGarbage)
+							garbage++;
+						break;
+				}
+			}
+			return garbage;
 		}
 	}
 }
