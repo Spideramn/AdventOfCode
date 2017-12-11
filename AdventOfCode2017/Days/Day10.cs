baa2375686868ef0d0a5349af90819ca311713fb
Spideramn <Spideramn@users.noreply.github.com>
2017-12-11 08:19:11 +0100

    Day 10

--- AdventOfCode2017/Days/Day10.cs
+++ AdventOfCode2017/Days/Day10.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+
 namespace AdventOfCode2017.Days
 {
 	internal class Day10 : Day
@@ -9,12 +12,63 @@ namespace AdventOfCode2017.Days
 
 		public override object RunPart1()
 		{
-			return null;
+			var curPos = 0;
+			var skip = 0;
+			var list = Enumerable.Range(0, 256).ToArray();
+			foreach (var length in Input.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse))
+			{
+				var left = curPos;
+				var right = curPos + length - 1;
+				while (left < right)
+				{
+					var t = list[left % 256];
+					list[left % 256] = list[right % 256];
+					list[right % 256] = t;
+					left++;
+					right--;
+				}
+				curPos += length + skip++;
+			}
+			return list[0] * list[1];
 		}
 
 		public override object RunPart2()
 		{
-			return null;
+			var curPos = 0;
+			var skip = 0;
+			var list = Enumerable.Range(0, 256).ToArray();
+			var input = Input.Select(c => (int)c).ToList();
+			input.AddRange(new[] {17, 31, 73, 47, 23});
+			for (var round = 0; round < 64; round++)
+			{
+				foreach (var length in input)
+				{
+					var left = curPos;
+					var right = curPos + length - 1;
+					while (left < right)
+					{
+						var t = list[left % 256];
+						list[left % 256] = list[right % 256];
+						list[right % 256] = t;
+						left++;
+						right--;
+					}
+					curPos += length + skip++;
+				}
+			}
+
+			var hash = "";
+			var temp = 0;
+			for (var index = 0; index < list.Length; index++)
+			{
+				if (index != 0 && index % 16 == 0)
+				{
+					hash += temp.ToString("x2");
+					temp = 0;
+				}
+				temp ^= list[index];
+			}
+			return hash + temp.ToString("x2"); 
 		}
 	}
 }
