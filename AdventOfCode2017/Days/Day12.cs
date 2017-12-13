62c8a0c0fb795dde7d9371dd8d746c654ef674b1
Spideramn <Spideramn@users.noreply.github.com>
2017-12-13 10:09:55 +0100

    Day 12

--- AdventOfCode2017/Days/Day12.cs
+++ AdventOfCode2017/Days/Day12.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace AdventOfCode2017.Days
 {
 	internal class Day12 : Day
@@ -9,12 +13,56 @@ namespace AdventOfCode2017.Days
 
 		public override object RunPart1()
 		{
-			return null;
+			var pipes = new Dictionary<int, int[]>();
+			foreach (var line in Input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+			{
+				var parts = line.Split("<->", StringSplitOptions.RemoveEmptyEntries);
+				pipes.Add(int.Parse(parts[0]), parts[1].Split(',').Select(int.Parse).ToArray());
+			}
+
+			var checkedPipes = new HashSet<int>();
+			var q = new Queue<int[]>(new[] {pipes[0]});
+			while (q.Any())
+			{
+				foreach (var pName in q.Dequeue())
+				{
+					if(checkedPipes.Add(pName))
+						q.Enqueue(pipes[pName]);
+				}
+			}
+			return checkedPipes.Count;
 		}
+		
 
 		public override object RunPart2()
 		{
-			return null;
+			var pipes = new Dictionary<int, int[]>();
+			foreach (var line in Input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+			{
+				var parts = line.Split("<->", StringSplitOptions.RemoveEmptyEntries);
+				pipes.Add(int.Parse(parts[0]), parts[1].Split(',').Select(int.Parse).ToArray());
+			}
+
+			var groupCount = 0;
+			var pipeNames = pipes.Keys.ToHashSet();
+			while (pipeNames.Any())
+			{
+				var pipesInGroup = new HashSet<int>();
+				var q = new Queue<int[]>(new[] {pipes[pipeNames.First()]});
+				while (q.Any())
+				{
+					foreach (var pName in q.Dequeue())
+					{
+						if (pipesInGroup.Add(pName))
+						{
+							pipeNames.Remove(pName);
+							q.Enqueue(pipes[pName]);
+						}
+					}
+				}
+				groupCount++;
+			}
+			return groupCount;
 		}
 	}
 }
