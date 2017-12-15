7763eaf17b3a5e5e8ef64c3b37d59832524844dc
Spideramn <Spideramn@users.noreply.github.com>
2017-12-15 16:32:44 +0100

    Day 15

--- AdventOfCode2017/Days/Day15.cs
+++ AdventOfCode2017/Days/Day15.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace AdventOfCode2017.Days
 {
 	internal class Day15 : Day
@@ -9,12 +15,50 @@ namespace AdventOfCode2017.Days
 
 		public override object RunPart1()
 		{
-			return null;
+			var parts = Input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries)).ToArray();
+
+			var startA = long.Parse(parts[0].Last());
+			var startB = long.Parse(parts[1].Last());
+			var count = 0;
+			for (var i = 0; i < 40000000; i++)
+			{
+				startA = ((startA * 16807L) % 2147483647L);
+				startB = ((startB * 48271L) % 2147483647L);
+
+				var a = BitConverter.GetBytes(startA);
+				var b = BitConverter.GetBytes(startB);
+				if (a[0] == b[0] && a[1] == b[1])
+					count++;
+			}
+			return count;
 		}
 
 		public override object RunPart2()
 		{
-			return null;
+			var parts = Input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries)).ToArray();
+
+			var startA = long.Parse(parts[0].Last());
+			var startB = long.Parse(parts[1].Last());
+			var count = 0;
+			for (var i = 0; i < 5000000; i++)
+			{
+				startA = GetValue(startA, 16807L, 2147483647L, 4);
+				startB = GetValue(startB, 48271L, 2147483647L, 8);
+				var a = BitConverter.GetBytes(startA);
+				var b = BitConverter.GetBytes(startB);
+				if (a[0]==b[0] && a[1]==b[1])
+					count++;
+			}
+			return count;
+		}
+
+		private static long GetValue(long start, long factor, long remainder, int multiple)
+		{
+			do
+			{
+				start = ((start * factor) % remainder);
+			} while (start % multiple != 0);
+			return start;
 		}
 	}
 }
