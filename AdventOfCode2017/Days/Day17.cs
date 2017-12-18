35061a9b1f0249e8b8ecaef0f8ba2eabdc678ab9
Spideramn <Spideramn@users.noreply.github.com>
2017-12-18 12:08:24 +0100

    Day 17

--- AdventOfCode2017/Days/Day17.cs
+++ AdventOfCode2017/Days/Day17.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace AdventOfCode2017.Days
 {
 	internal class Day17 : Day
@@ -9,12 +11,32 @@ namespace AdventOfCode2017.Days
 
 		public override object RunPart1()
 		{
-			return null;
+			var stepSize = int.Parse(Input);
+			var buffer = new int[2017 + 1];
+			var pos = 0;
+			var length = 1;
+			while (length < buffer.Length)
+			{
+				pos = ((pos + stepSize) % length) + 1;
+				if (length != pos)
+					Array.Copy(buffer, pos, buffer, pos + 1, buffer.Length - (pos + 1));
+				buffer[pos] = length++;
+			}
+			return buffer[pos + 1];
 		}
 
 		public override object RunPart2()
 		{
-			return null;
+			var stepSize = int.Parse(Input);
+			var pos = 0;
+			var target = 0;
+			for (var length = 1; length <= 50000000; length++)
+			{
+				pos = ((pos + stepSize) % length) + 1;
+				if (pos == 1) // only interested in pos 1
+					target = length;
+			}
+			return target;
 		}
 	}
 }
